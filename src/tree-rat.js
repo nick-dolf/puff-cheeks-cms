84133eb2975c9b9ad5d50fc2bdca3b765754c4ad
nick-dolf <nickdolf@gmail.com>
2023-09-18 09:11:19 -0500

    added folders and locals variables

--- src/tree-rat.js
+++ src/tree-rat.js
@@ -4,12 +4,15 @@ const fse = require("fs-extra");
 const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
+module.exports = app;
 const chokidar = require("chokidar");
 
 fse.ensureDirSync("pages");
 fse.ensureDirSync("site");
 
 require("./utils/config").verify();
+require("./utils/setup").variables();
+require("./utils/setup").folders();
 
 fse.outputFile("site/index.html", `<h1>Site</h1>
                                    <p>Hello World</p>`);
@@ -25,8 +28,7 @@ app.listen(PORT, () => {
   console.log(`Port: ${PORT}`);
   console.log(`Site: http://localhost:${PORT}/`);
   console.log(`Admin: http://localhost:${PORT}/admin`);
-  //localhost:7533/
-  http: console.log("--");
+  console.log("--");
 });
 
 
---
+++ src/utils/setup.js
@@ -0,0 +1,70 @@
+const fse = require("fs-extra");
+const app = require("../tree-rat");
+const path = require("path")
+const PuffCheeksStorage = require("puff-cheeks")
+
+/**
+ * Sets up local variables
+ */
+function variables() {
+  const config = fse.readJsonSync("tree-rat.json");
+
+  app.locals.site = {};
+
+  // Site Url
+  if (process.env.NODE_ENV === "development") {
+    app.locals.site.url = "/";
+    app.locals.env = "development";
+  } else if (process.env.NODE_ENV === "staging") {
+    app.locals.site.url = config.stagingUrl;
+    app.locals.env = "staging";
+  } else {
+    app.locals.site.url = config.productionUrl;
+    app.locals.env = "production";
+  }
+
+  app.locals.enableGzip = config.enableGzipCompression;
+  app.locals.autoPrefixer = config.enableAutoPrefixer;
+
+  // Directory paths
+  app.locals.siteDir = path.join(process.cwd(), "site");
+  app.locals.pageDir = path.join(process.cwd(), "pages");
+  app.locals.viewDir = path.join(process.cwd(), "views");
+  app.locals.imgDir = path.join(process.cwd(), "images");
+  app.locals.fileDir = path.join(process.cwd(), "files");
+
+  // Setup Json "database"
+  app.locals.pages = new PuffCheeksStorage("pages", "link");
+  app.locals.site.pages = app.locals.pages.data;
+
+  app.locals.folders = new PuffCheeksStorage("folders", "slug")
+  app.locals.site.folders = app.locals.folders.data;
+
+  app.locals.blocks = new PuffCheeksStorage("blocks", "slug");
+  app.locals.site.blocks = app.locals.blocks.data;
+
+  app.locals.sections = new PuffCheeksStorage("sections", "slug");
+  app.locals.site.sections = app.locals.sections.data;
+
+  app.locals.images = new PuffCheeksStorage("images", "name");
+  app.locals.site.images = app.locals.images.data;
+
+  app.locals.files = new PuffCheeksStorage("files", "name");
+  app.locals.site.files = app.locals.files.data;
+
+  app.locals.users = new PuffCheeksStorage("users", "email", "secrets");
+  app.locals.site.users = app.locals.users.data;
+}
+
+function folders() {
+  fse.ensureDirSync(app.locals.siteDir);
+  fse.ensureDirSync(app.locals.siteDir + "/assets/images");
+  fse.ensureDirSync(app.locals.pageDir);
+  fse.ensureDirSync(app.locals.pageDir + "/drafts");
+  fse.ensureDirSync(app.locals.pageDir + "/published");
+  fse.ensureDirSync(app.locals.viewDir);
+  fse.ensureDirSync(app.locals.imgDir);
+  fse.ensureDirSync(app.locals.fileDir);
+}
+
+module.exports = { variables, folders };
